a7af994a0bbe6d9d08c88cb0137f9b5c5dbd7b8f
xJefi <jefersonferrari78@gmail.com>
2026-06-06 23:01:28 -0300

    configuração da conexão com o banco de dados

---
+++ src/config/db.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql2/promise');
+require('dotenv').config();
+
+// Cria a conexão com o banco
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0,
+});
+
+module.exports = pool;
