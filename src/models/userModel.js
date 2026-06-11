a7574180c21a607eb1e5f02aa47aceddd84793a4
xJefi <jefersonferrari78@gmail.com>
2026-06-10 21:38:33 -0300

    Adicionando model de usuários

---
+++ src/models/userModel.js
@@ -0,0 +1,57 @@
+const db = require('../config/db');
+
+class UserModel {
+  static async create(userData) {
+    const { name, email, password, phone, role } = userData;
+
+    const [result] = await db.query(
+      'INSERT INTO users (name, email, password, phone, role) VALUES (?, ?, ?, ?, ?)',
+      [name, email, password, phone, role],
+    );
+
+    return result.insertId;
+  }
+
+  //Buscar todos
+  static async findAll() {
+    const [rows] = await db.query(
+      'SELECT id, name, email, phone, role FROM users',
+    );
+
+    return rows;
+  }
+
+  //Buscar por email
+  static async findByEmail(email) {
+    const [rows] = await db.query('SELECT * FROM users WHERE email = ?', [
+      email,
+    ]);
+
+    return rows[0];
+  }
+
+  //Buscar por ID
+  static async findById(id) {
+    const [rows] = await db.query(
+      'SELECT id, name, email, phone, role FROM users WHERE id = ?',
+      [id],
+    );
+
+    return rows[0];
+  }
+
+  //Atualizar usuário existente
+  static async update(id, { name, email, phone }) {
+    await db.query(
+      'UPDATE users SET name = ?, email = ?, phone = ? WHERE id = ?',
+      [name, email, phone, id],
+    );
+  }
+
+  //Deletar usuário
+  static async delete(id) {
+    await db.query('DELETE FROM users WHERE id = ?', [id]);
+  }
+}
+
+module.exports = UserModel;
