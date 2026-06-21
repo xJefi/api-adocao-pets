759497bedcd69a70c8bc04ba74dac2f365b4ffc5
xJefi <jefersonferrari78@gmail.com>
2026-06-21 17:02:16 -0300

    importando a rota de pets no app.js

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const helmet = require('helmet');
 const authRoutes = require('./routes/authRoutes');
 const userRoutes = require('./routes/userRoutes');
+const petRoutes = require('./routes/petRoutes');
 
 const app = express();
 
@@ -14,5 +15,6 @@ app.use(express.json());
 // Rotas da API
 app.use('/auth', authRoutes);
 app.use('/users', userRoutes);
+app.use('/pets', petRoutes);
 
 module.exports = app;
