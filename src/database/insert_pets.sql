3f5a96fae06f4bf4a27c96a24da49d4307016128
xJefi <jefersonferrari78@gmail.com>
2026-06-26 20:57:16 -0300

    separando criacao do banco e inserts

---
+++ src/database/insert_pets.sql
@@ -0,0 +1,27 @@
+USE pets_db;
+
+INSERT INTO pets (name, age, species, size, status, description) VALUES
+    (
+        'Frederico',
+        1,
+        'dog',
+        'small',
+        'available',
+        'Cachorrinho travesso e brincalhão.'
+    ),
+    (
+        'Tom',
+        3,
+        'cat',
+        'large',
+        'available',
+        'Gatinho dócil, brincalhão e muito inteligente.'
+    ),
+    (
+        'Niko',
+        1,
+        'cat',
+        'medium',
+        'available',
+        'Gatinho carinhoso e tranquilo.'
+    );
