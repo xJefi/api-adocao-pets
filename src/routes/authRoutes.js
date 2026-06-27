64b03beeb31ad3d6ca0c1e40963240f894f89796
xJefi <jefersonferrari78@gmail.com>
2026-06-26 22:02:03 -0300

    update rotas de autenticacao

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,7 +3,6 @@ const AuthController = require('../controllers/authController');
 
 const router = express.Router();
 
-//router.get('/pets/available', ) FALTA IMPLEMENTAR
 router.post('/users', AuthController.register);
 router.post('/login', AuthController.login);
 
